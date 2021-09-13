c40d0b73ea9eb74291d3c3ac92131381aed62264
Valter Núñez <valter.nunez@outlook.com>
2021-09-13 16:14:43 -0500

    Update models.py

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=500)
+    def __str__(self):
+        return self.brand_name
+
+class Product(models.Model):
+    sku = models.PositiveIntegerField(primary_key=True, validators=[MaxValueValidator(999999999999999)])
+    product_name = models.CharField(max_length=500)
+    product_price = models.DecimalField( max_digits=50, decimal_places=2)
+    product_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    times_searched_anonymous = models.PositiveIntegerField(default=0)
+
+
+    def __str__ (self):
+        return self.product_name + ' (' + self.product_brand.brand_name + ')' + ' - $' + str(self.product_price)
