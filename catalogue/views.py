e5f74d1f1e1e9df1bc1ea145ee266a9a123f6cef
Valter Núñez <valter.nunez@outlook.com>
2021-09-13 17:03:02 -0500

    User Administration views.py
    Added administration for Users

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+#
+# REST Views for User, Product and Brand.
+# GET
+# POST
+# UPDATE
+# DELETE
+#
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework.generics import CreateAPIView
+from .serializers import ProductSerializer, BrandSerializer, UserSerializer
+from rest_framework.authtoken.admin import User
 
-# Create your views here.
+
+# User administration
+class CreateUserView(CreateAPIView):
+    model = get_user_model()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Product Administration
+
+# Brand Administration
